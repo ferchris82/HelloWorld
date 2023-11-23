d7d9661c772850dba1aefeb1019474bdd3e01b20
Christian <ferchris82@hotmail.com>
2023-11-22 22:16:37 -0500

    Version 1.4

--- src/OperadoresLogicos.java
+++ src/OperadoresLogicos.java
@@ -2,7 +2,7 @@ public class OperadoresLogicos {
     public static void main(String[] args) {
         
         int i = 3; 
-        byte j = 7;
+        byte j = 3;
         float k = 127e-7f;
         double l = 2.1413e3;
         boolean m = false;
@@ -15,5 +15,18 @@ public class OperadoresLogicos {
 
         boolean b3 = i == j && (k < l || m == false);
         System.out.println("b3 = " + b3);
+
+        boolean b4 = (i == j || k < l) && m == true;
+        System.out.println("b4 = " + b4);
+        
+        boolean b5 = (true || true) && false;
+        System.out.println("b5 = " + b5);
+
+        boolean b6 = true || false && false || false; // false
+        System.out.println("b6 = " + b6);
+        
+        boolean b7 = ((true || false && false)) || false; // false
+        System.out.println("b7 = " + b7);
     }
+
 }
