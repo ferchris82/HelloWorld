d773826db25194e64ee940321b05f6e58cd17f08
Christian <ferchris82@hotmail.com>
2023-11-23 17:32:09 -0500

    Version 1.5

---
+++ src/SentenciaForArreglo.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class SentenciaForArreglo {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Andres","Pepe","Maria","Paco","Pepa","Lalo","Bea","Pato"};
+        int count = nombres.length;
+        for(int i = 0; i < count; i++){
+            if(nombres[i].toLowerCase().contains("Andres".toLowerCase()) ||
+                nombres[i].toLowerCase().contains("pePa".toLowerCase())){
+                continue;
+            }
+            System.out.println(i + ".- " + nombres[i]);
+        }
+
+        String buscar = JOptionPane.showInputDialog("Ingrese un nombre, ejemplo \"Pepe\" o \"Maria\":");
+        System.out.println("buscar = " + buscar);
+
+        boolean encontrado = false;
+        for (int i = 0; i < count; i++){
+            if(nombres[i].equalsIgnoreCase(buscar)){
+                encontrado = true;
+                break;
+            }
+            System.out.println("nombres = " + nombres[i]);
+        }
+        if(encontrado){
+            JOptionPane.showMessageDialog(null, buscar + " fue encontrado!");
+        } else {
+            JOptionPane.showMessageDialog(null, buscar + " no existe en el sistema!");
+
+        }
+    }
+}
--- src/SentenciaSwitchCaseNumDiasMes.java
+++ src/SentenciaSwitchCaseNumDiasMes.java
@@ -11,16 +11,32 @@ public class SentenciaSwitchCaseNumDiasMes {
         System.out.println("Ingrese el año (YYYY)");
         int anio = s.nextInt();
 
-        if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12){
-            numeroDias = 31;
-        } else if(mes == 4 || mes == 6 || mes == 9 || mes == 11){
-            numeroDias = 30;
-        } else if(mes == 2){
-            if(anio % 400 == 0 || ((anio % 4 == 0) && !(anio % 100 == 0))){
-                numeroDias = 29;
-            } else {
-                numeroDias = 28;
-            }
+        switch(mes){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                numeroDias = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                numeroDias = 30;
+                break;
+            case 2:
+                if(anio % 400 == 0 || ((anio % 4 == 0) && !(anio % 100 == 0))){
+                    numeroDias = 29;
+                } else {
+                    numeroDias = 28;
+                }
+                break;
+            default:
+                numeroDias = 0;
+
         }
         System.out.println("numeroDias = " + numeroDias);
     }
