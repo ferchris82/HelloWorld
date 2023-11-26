5ef09ff83cb7f536403e5244af20b35cdaa2b7f8
Christian <ferchris82@hotmail.com>
2023-11-26 16:51:08 -0500

    Version 1.6

--- src/SentenciaForEach.java
+++ src/SentenciaForEach.java
@@ -1,7 +1,7 @@
 public class SentenciaForEach {
     public static void main(String[] args) {
 
-        int[] numeros = {1, 3, 5, 7, 9, 11, 13, 14};
+        int[] numeros = {1, 3, 5, 7, 9, 11, 13, 15};
         for(int num: numeros){
             System.out.println("num = " + num);
         }
